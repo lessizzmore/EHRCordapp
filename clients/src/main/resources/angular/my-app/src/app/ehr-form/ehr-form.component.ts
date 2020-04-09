1390a0b90762916c75c40b03d18f6562e49c1a0d
Zhenzhen Tan <zhenzhen.tan@r3.com>
2020-04-09 11:27:29 -0400

    This is the last working version

--- clients/src/main/resources/angular/my-app/src/app/ehr-form/ehr-form.component.ts
+++ clients/src/main/resources/angular/my-app/src/app/ehr-form/ehr-form.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { EhrService } from '../services/ehr.service';
 import { FormBuilder } from '@angular/forms';
 import { EHR } from '../ehr';
+import { UploadService } from '../services/upload.service';
+import { map, catchError } from 'rxjs/operators';
+import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
+import { of } from 'rxjs';
 
 
 @Component({
@@ -12,7 +16,11 @@ import { EHR } from '../ehr';
 export class EhrFormComponent implements OnInit {
   ehrForm;
   ehr: EHR;
-  constructor(private ehrSvc: EhrService, private formBuilder: FormBuilder) {
+  @ViewChild("fileUpload", {static: false}) fileUpload: ElementRef;
+  files  = [];  
+
+
+  constructor(private ehrSvc: EhrService, private formBuilder: FormBuilder,private uploadService: UploadService) {
     this.ehrForm = this.formBuilder.group({
       patient: '',
       targetDoctor: '',
@@ -38,5 +46,48 @@ export class EhrFormComponent implements OnInit {
     );
     console.warn('Your form has been submitted', data);
   }
+
+  uploadFile(file) {  
+    const formData = new FormData();  
+    formData.append('file', file.data);  
+    file.inProgress = true;  
+    this.uploadService.upload(formData).pipe(  
+      map(event => {  
+        switch (event.type) {  
+          case HttpEventType.UploadProgress:  
+            file.progress = Math.round(event.loaded * 100 / event.total);  
+            break;  
+          case HttpEventType.Response:  
+            return event;  
+        }  
+      }),  
+      catchError((error: HttpErrorResponse) => {  
+        file.inProgress = false;  
+        return of(`${file.data.name} upload failed.`);  
+      })).subscribe((event: any) => {  
+        if (typeof (event) === 'object') {  
+          console.log(event.body);  
+        }  
+      });  
+  }
+
+  uploadFiles() {  
+    this.fileUpload.nativeElement.value = '';  
+    this.files.forEach(file => {  
+      this.uploadFile(file);  
+    });  
+  }
+
+  onClick() {  
+    const fileUpload = this.fileUpload.nativeElement;fileUpload.onchange = () => {  
+    for (let index = 0; index < fileUpload.files.length; index++)  
+    {  
+     const file = fileUpload.files[index];  
+     this.files.push({ data: file, inProgress: false, progress: 0});  
+    }  
+      this.uploadFiles();  
+    };  
+    fileUpload.click();  
+}
 
 }
